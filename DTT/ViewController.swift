b2e74036ec8df17f497aa826d2cfd9562ce87d1c
Martin Herholdt Rasmussen <mhr@momondo.com>
2018-03-14 15:53:00 +0100

    Initial Commit

---
+++ DTT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DTT
+//
+//  Created by Martin Herholdt Rasmussen on 14/03/2018.
+//  Copyright © 2018 Martin Herholdt Rasmussen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
